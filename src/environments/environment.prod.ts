615b28eda0d678c2be527d04f7866b553b57bd0e
ashish_signity <ashish@signitysolutions.com>
2018-08-22 16:41:45 +0530

    small changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
+<<<<<<< Updated upstream
   apiUrl:'https://eth-web3-node.herokuapp.com/'
+=======
+  apiUrl:'http://192.168.1.72:3003/'
+>>>>>>> Stashed changes
 };
